8933a638365c10afeb77c7209ae9534b4acc7b0d
fkaaziebu <frederickaziebu1998@gmail.com>
2025-11-05 09:56:07 +0000

    change policy for courses page

--- src/app/(dashboard)/courses/page.tsx
+++ src/app/(dashboard)/courses/page.tsx
@@ -47,6 +47,7 @@ export default function StudentCoursesPage() {
         variables: {
           organizationId,
         },
+        fetchPolicy: "no-cache",
       });
 
       const coursesData =
