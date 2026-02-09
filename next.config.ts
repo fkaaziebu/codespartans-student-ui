2618e66fab2e3deea2c9d8542a383470904515d4
fkaaziebu <frederickaziebu1998@gmail.com>
2026-02-09 12:30:09 +0000

    add ip to sources for image fetching

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,10 @@ const nextConfig: NextConfig = {
         protocol: "http",
         hostname: "localhost",
       },
+      {
+        protocol: "http",
+        hostname: "138.68.167.20",
+      },
     ],
   },
   output: "standalone",
